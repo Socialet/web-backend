43c79b16cda85f2e5460dffe04230d1d7616f155
Prathamesh Kurunkar <kurunkarprathamesh7@gmail.com>
2022-01-26 13:54:44 +0530

    feat:hashtags api for multiple images & cors error

--- app/routers/hashtags.py
+++ app/routers/hashtags.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter,UploadFile,File
+from typing import List, Optional
 from app.toolkit.HashtagRecommender.model import HashtagRecommenderModel
 from app.utils.image_processing import read_image
 
 hashtags_view = APIRouter()
 
 @hashtags_view.post("/recommend")
-async def recommend(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    if not extension:
-        return "Image must be jpg or png format!"
-    image = read_image(await file.read())
-    model = HashtagRecommenderModel()
-    hashtags=model.recommend_hashtags(image)
-
+async def recommend(files: Optional[List[UploadFile]] = File(None)):
+    hashtags = []
+    for file in files:
+        extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+        if not extension:
+            return "Image must be jpg or png format!"
+        image = read_image(await file.read())
+        model = HashtagRecommenderModel()
+        image_hashtags=model.recommend_hashtags(image)
+        hashtags.extend(image_hashtags)
+    hashtags = list(set(hashtags))
     return {
-        "data":{
-            "hashtags":hashtags
-        }
+        "hashtags":hashtags
     }
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import uvicorn
 from app.api import app_api
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+middleware = [Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])]
 
 app = FastAPI(
     title="SMWT",
     version="1.0",
     description="All in one Social Media Workflow Tool",
-)
-
-origins = ['http://localhost:3000']
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    middleware=middleware
 )
 
 app.include_router(app_api,prefix="/api",tags=["ALL API"])
